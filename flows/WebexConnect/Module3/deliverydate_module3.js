0ad34e02c1aaf78c15d851fe9b06253d8dc7964c
webexcpaas <156833461+webexcpaas@users.noreply.github.com>
2025-02-06 16:10:43 +0000

    Add files via upload

---
+++ flows/WebexConnect/Module3/deliverydate_module3.js
@@ -0,0 +1,9 @@
+var d1 = new Date();
+d1.setDate(d1.getDate() + 4);
+var date1 = d1.toDateString();
+var d2 = new Date();
+d2.setDate(d2.getDate() + 5);
+var date2 = d2.toDateString();
+var d3 = new Date();
+d3.setDate(d3.getDate() + 6);
+var date3 = d3.toDateString();
